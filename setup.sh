647a9944c37a2b0ca9a05f5f674ac8fe51e37ff7
Abdullah Al-Qubli <awa37@mail.aub.edu>
2026-03-29 20:43:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-echo "=== Setting up Dots and Boxes ==="
+echo "=== Setting up game ==="
+
+apk add build-base git
 
 make
 
-sed -i 's|^tty1::.*|tty1::respawn:/sbin/getty --autologin root 38400 tty1|' /etc/inittab
+sed -i 's|^tty1::.*|tty1::respawn:/sbin/getty -n -l /bin/sh 38400 tty1|' /etc/inittab
 
 GAME_PATH="$(pwd)/dots_and_boxes"
-
 grep -qxF "$GAME_PATH" /root/.profile || echo "$GAME_PATH" >> /root/.profile
 
 echo "=== Done! Run: reboot ==="
