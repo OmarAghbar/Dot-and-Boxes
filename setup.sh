ed0a4a069ad28399f46948228ec2efe09132c38e
awx-06 <awa37@mail.aub.edu>
2026-03-29 17:35:32 +0300

    minor bug fixes

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo bash -c 'cat > /etc/systemd/system/getty@tty1.service.d/override.conf << EO
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin root --noclear %I \$TERM
-EOF'
+EOF'gi
 
 grep -qxF "$GAME_PATH" /root/.profile || echo "$GAME_PATH" | sudo tee -a /root/.profile
 
