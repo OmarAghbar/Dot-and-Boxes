28ed2fd34016771de9b4eb458dfc65e7b27691d5
Abdullah Al-Qubli <awa37@mail.aub.edu>
2026-03-29 21:13:04 +0300

    Update GAME_PATH in setup.sh for tty1 respawn

--- setup.sh
+++ setup.sh
@@ -6,9 +6,7 @@ apk add build-base git
 
 make
 
-sed -i 's|^tty1::.*|tty1::respawn:/sbin/getty -n -l /bin/sh 38400 tty1|' /etc/inittab
-
 GAME_PATH="$(pwd)/dots_and_boxes"
-grep -qxF "$GAME_PATH" /root/.profile || echo "$GAME_PATH" >> /root/.profile
+sed -i "s|^tty1::.*|tty1::respawn:$GAME_PATH|" /etc/inittab
 
 echo "=== Done! Run: reboot ==="
